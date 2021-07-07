ac51b3ae2c81b9782d3c2111cba51ed731d778e7
Derek Su <dereksu@qnap.com>
2021-07-07 15:41:15 +0800

    Add fio scripts

---
+++ batch-run.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+NAME=fio-`date +%s`
+
+IODEPTH=64
+FILESIZE="124G"
+RUNTIME=900
+NUMJOBS=4
+LOOPS=3
+
+WORKING_DIR=`pwd`/${NAME}
+[ ! -e ${WORKING_DIR} ] || rm -rf ${WORKING_DIR}
+mkdir -p ${WORKING_DIR}
+
+for DIRECT in 0 1
+do
+	for BS in "64K" "512K"
+	do
+		OUTPUT_DIR=`pwd`/${NAME}/${DIRECT}/${BS}
+
+		[ ! -e ${OUTPUT_DIR} ] || rm -rf ${OUTPUT_DIR}
+		mkdir -p ${OUTPUT_DIR}
+
+		bash fio-run.sh --runtime ${RUNTIME} --iodepth ${IODEPTH} --size ${FILESIZE} --blocksize ${BS} --direct ${DIRECT} --numjobs ${NUMJOBS} --loops ${LOOPS} --output ${OUTPUT_DIR} ${WORKING_DIR} fio-tests/*.fio
+	done
+done
+
+rm -f ${WORKING_DIR}/testfile
---
+++ fio-run.sh
@@ -0,0 +1,146 @@
+#!/bin/bash
+
+# Defaults
+RAMP_TIME=30
+RUNTIME=120
+LOOPS=3
+IODEPTH=64
+SIZE="20G"
+NUMJOBS=1
+BLOCKSIZE="4K"
+DIRECT=0
+
+drop_cache()
+{
+    echo 3 > /proc/sys/vm/drop_caches
+}
+
+print_info() {
+    cat <<-FOE
+    JOBNAME="$1"
+    LOOP="$2"
+    TEST_DIR=${TEST_DIR}
+    RAMP_TIME=${RAMP_TIME}
+    RUNTIME=${RUNTIME}
+    LOOPS=${LOOPS}
+    NUMJOBS=${NUMJOBS}
+    IODEPTH=${IODEPTH}
+    SIZE=${SIZE}
+    BLOCKSIZE=${BLOCKSIZE}
+    DIRECT=${DIRECT}
+    OUTPUT_DIR=${OUTPUT_DIR}
+FOE
+}
+
+run_test()
+{
+    local LOOP="$1"
+    local JOB_FILE="$2"
+    local BASENAME=$(basename ${JOB_FILE} | cut -f 1 -d '.')
+
+    local OUTPUT="fio_ramptime_${RAMP_TIME}_runtime_${RUNTIME}_numjobs_${NUMJOBS}_iodepth_${IODEPTH}_direct_${DIRECT}_bs_${BLOCKSIZE}_${BASENAME}.${LOOP}"
+
+    drop_cache
+
+    fio --directory="$TEST_DIR" --ramp_time=$RAMP_TIME --runtime=$RUNTIME --iodepth=$IODEPTH --size="$SIZE" --direct=$DIRECT --blocksize="$BLOCKSIZE" --numjobs=$NUMJOBS --ioengine=libaio --output "$OUTPUT_DIR/$OUTPUT" --output-format=json "$JOB_FILE"
+}
+
+usage()
+{
+    CMD=$1
+
+    cat <<-FOE
+Usage: $CMD: [OPTIONS] <test-directory> <fio-job> ....
+Run fio tests
+Options:
+    -h | --help   Print help message
+    --ramp_time   Time in seconds for ramp time. Default is 30 seconds.
+    --runtime     Time in seconds for which each job should run. Default is 90 seconds.
+    --loops       Number of times each job is run. Default is 3.
+    --iodepth     Number of I/O units to keep in flight. Default is 64.
+    --size        Total size of file. Default is 20G.
+    --blocksize   Specify blocksize. Default is 4K.
+    --direct      If set to 1, use non-buffered I/O. Default is 0.
+
+    --output      Path to output directory.
+FOE
+}
+
+# Main logic
+
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+case $key in
+    -h|--help)
+    usage
+    exit 0
+    ;;
+    -r|--ramp_time)
+    RAMP_TIME="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -t|--runtime)
+    RUNTIME="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -l|--loops)
+    LOOPS="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -n|--numjobs)
+    NUMJOBS="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -d|--iodepth)
+    IODEPTH="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -s|--size)
+    SIZE="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -b|--blocksize)
+    BLOCKSIZE="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -t|--direct)
+    DIRECT="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -o|--output)
+    OUTPUT_DIR="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    *)    # unknown option
+    POSITIONAL+=("$1") # save it in an array for later
+    shift # past argument
+    ;;
+esac
+done
+set -- "${POSITIONAL[@]}" # restore positional parameters
+
+TEST_DIR="$1"
+
+shift 1
+
+JOB_FILES="$@"
+
+for jobname in $JOB_FILES;do
+    for i in `seq 1 $LOOPS`;do
+        echo ">>>>>>>>>> $jobname #$i <<<<<<<<<<"
+        print_info "$jobname" "$i"
+        run_test "$i" "$jobname"
+    done
+done
