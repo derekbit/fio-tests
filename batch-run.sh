28d1f874a251d91eb90448606673e90d3e8a685f
Derek Su <dereksu@qnap.com>
2021-07-14 12:04:13 +0800

    Change bs for random and sequential rw
    Use 4K block size for random io, and 1M for sequential io.

--- batch-run.sh
+++ batch-run.sh
@@ -3,26 +3,40 @@
 NAME=fio-`date +%s`
 
 IODEPTH=64
-FILESIZE="124G"
-RUNTIME=900
-NUMJOBS=4
-LOOPS=3
+FILESIZE="96G"
+RUNTIME=180
+LOOPS=1
 
 WORKING_DIR=`pwd`/${NAME}
 [ ! -e ${WORKING_DIR} ] || rm -rf ${WORKING_DIR}
 mkdir -p ${WORKING_DIR}
 
-for DIRECT in 0 1
+for DIRECT in 0
 do
-	for BS in "64K" "512K"
+	# Sequential
+	NUMJOBS=1
+	for BS in "1M"
 	do
 		OUTPUT_DIR=`pwd`/${NAME}/${DIRECT}/${BS}
 
 		[ ! -e ${OUTPUT_DIR} ] || rm -rf ${OUTPUT_DIR}
 		mkdir -p ${OUTPUT_DIR}
 
-		bash fio-run.sh --runtime ${RUNTIME} --iodepth ${IODEPTH} --size ${FILESIZE} --blocksize ${BS} --direct ${DIRECT} --numjobs ${NUMJOBS} --loops ${LOOPS} --output ${OUTPUT_DIR} ${WORKING_DIR} fio-tests/*.fio
+		bash fio-run.sh --runtime ${RUNTIME} --iodepth ${IODEPTH} --size ${FILESIZE} --blocksize ${BS} --direct ${DIRECT} --numjobs ${NUMJOBS} --loops ${LOOPS} --output ${OUTPUT_DIR} ${WORKING_DIR} fio-tests/seq/*.fio
 	done
+
+	# Random
+	NUMJOBS=4
+	for BS in "4K"
+	do
+		OUTPUT_DIR=`pwd`/${NAME}/${DIRECT}/${BS}
+
+		[ ! -e ${OUTPUT_DIR} ] || rm -rf ${OUTPUT_DIR}
+		mkdir -p ${OUTPUT_DIR}
+
+		bash fio-run.sh --runtime ${RUNTIME} --iodepth ${IODEPTH} --size ${FILESIZE} --blocksize ${BS} --direct ${DIRECT} --numjobs ${NUMJOBS} --loops ${LOOPS} --output ${OUTPUT_DIR} ${WORKING_DIR} fio-tests/rand/*.fio
+	done
+
 done
 
 rm -f ${WORKING_DIR}/testfile
