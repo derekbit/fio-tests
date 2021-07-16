2bfa2d719baa0d5db596243089c14921f34c1e17
Derek Su <dereksu@qnap.com>
2021-07-16 12:09:04 +0800

    Fix batch-run.sh

--- batch-run.sh
+++ batch-run.sh
@@ -6,37 +6,30 @@ IODEPTH=64
 FILESIZE="96G"
 RUNTIME=180
 LOOPS=1
+DIRECT=0
 
 WORKING_DIR=`pwd`/${NAME}
 [ ! -e ${WORKING_DIR} ] || rm -rf ${WORKING_DIR}
 mkdir -p ${WORKING_DIR}
 
-for DIRECT in 0
+# Sequential
+NUMJOBS=1
+for BS in "1M"
 do
-	# Sequential
-	NUMJOBS=1
-	for BS in "1M"
-	do
-		OUTPUT_DIR=`pwd`/${NAME}/${DIRECT}/${BS}
+	OUTPUT_DIR=`pwd`/${NAME}/${DIRECT}/${BS}
+	mkdir -p ${OUTPUT_DIR}
 
-		[ ! -e ${OUTPUT_DIR} ] || rm -rf ${OUTPUT_DIR}
-		mkdir -p ${OUTPUT_DIR}
+	bash fio-run.sh --runtime ${RUNTIME} --iodepth ${IODEPTH} --size ${FILESIZE} --blocksize ${BS} --direct ${DIRECT} --numjobs ${NUMJOBS} --loops ${LOOPS} --output ${OUTPUT_DIR} ${WORKING_DIR} fio-tests/seq/*.fio
+done
 
-		bash fio-run.sh --runtime ${RUNTIME} --iodepth ${IODEPTH} --size ${FILESIZE} --blocksize ${BS} --direct ${DIRECT} --numjobs ${NUMJOBS} --loops ${LOOPS} --output ${OUTPUT_DIR} ${WORKING_DIR} fio-tests/seq/*.fio
-	done
+# Random
+NUMJOBS=4
+for BS in "4K"
+do
+	OUTPUT_DIR=`pwd`/${NAME}/${DIRECT}/${BS}
+	mkdir -p ${OUTPUT_DIR}
 
-	# Random
-	NUMJOBS=4
-	for BS in "4K"
-	do
-		OUTPUT_DIR=`pwd`/${NAME}/${DIRECT}/${BS}
-
-		[ ! -e ${OUTPUT_DIR} ] || rm -rf ${OUTPUT_DIR}
-		mkdir -p ${OUTPUT_DIR}
-
-		bash fio-run.sh --runtime ${RUNTIME} --iodepth ${IODEPTH} --size ${FILESIZE} --blocksize ${BS} --direct ${DIRECT} --numjobs ${NUMJOBS} --loops ${LOOPS} --output ${OUTPUT_DIR} ${WORKING_DIR} fio-tests/rand/*.fio
-	done
-
+	bash fio-run.sh --runtime ${RUNTIME} --iodepth ${IODEPTH} --size ${FILESIZE} --blocksize ${BS} --direct ${DIRECT} --numjobs ${NUMJOBS} --loops ${LOOPS} --output ${OUTPUT_DIR} ${WORKING_DIR} fio-tests/rand/*.fio
 done
 
 rm -f ${WORKING_DIR}/testfile
